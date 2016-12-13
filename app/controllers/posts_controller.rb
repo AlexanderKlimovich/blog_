940a5795010b39c4f21b34496b0aac2bc7390bbb
Alexandr <alexanderklimovich@gmail.com>
2016-12-13 16:54:32 +0200

    use flash messeges

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,9 +16,9 @@ before_action :set_post, only: [:show, :edit, :update, :destroy]
   def create
     @post = Post.new(post_params)
     if @post.save
-       redirect_to @post
+       redirect_to @post, success: 'Статья успешно создана!'
     else
-       render :new
+       render :new, denger: 'Статья не создана'
     end      
   end
 
@@ -27,15 +27,15 @@ before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def update
    if @post.update_attributes(post_params)
-   	 redirect_to @post
+   	 redirect_to @post, success: 'Статья успешно обновлена'
    else
-     render :edit
+     render :edit, denger: 'Статья не обновлена'
    end  	 
   end
 
   def destroy
     @post.destroy
-    redirect_to posts_path
+    redirect_to posts_path, success: 'Статья успешно удалена'
   end	
 
   private 
